b083fb3e2a7ac8080278d35c1c35723d66db2461
Wisnu Anggara <wisnuanggara05@gmail.com>
2020-07-18 21:04:54 +0700

    ïnitial xcode project creation

---
+++ yt-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yt-onedaybuild
+//
+//  Created by Wisnu Anggara on 18/07/20.
+//  Copyright © 2020 Untitled Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
